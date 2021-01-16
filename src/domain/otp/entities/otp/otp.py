16acf558e87934e53873b83e7fea685a085891fc
Bruno Lucattelli <lucattelli@gmail.com>
2021-01-16 18:53:37 -0300

    Generate and hash OTP

--- src/domain/otp/entities/otp/otp.py
+++ src/domain/otp/entities/otp/otp.py
@@ -1,7 +1,12 @@
 from dataclasses import dataclass
+from random import randint
+from typing import Optional
 from domain.otp.entities.otp.hash_abstract import HashAbstract
 from domain.otp.enums.otp_method_enum import OTPMethodEnum
 from domain.otp.enums.otp_status_enum import OTPStatusEnum
+from domain.otp.exceptions.otp_generation_failed_exception import (
+    OTPGenerationFailedException,
+)
 from domain.otp.exceptions.otp_verification_failed_exception import (
     OTPVerificationFailedException,
 )
@@ -11,9 +16,9 @@ from domain.otp.exceptions.otp_verification_failed_exception import (
 class OTP:
     method: OTPMethodEnum
     to: str
-    hashed_otp: bytes
     encoder: HashAbstract
     status: OTPStatusEnum
+    hashed_otp: Optional[bytes] = None
 
     def verify(self, otp_code: str) -> None:
         if self.__invalid_status_for_verification() or self.__incorrect_otp(
@@ -24,6 +29,13 @@ class OTP:
 
         self.status = OTPStatusEnum.VALIDATED.value
 
+    def generate_otp(self) -> str:
+        if self.hashed_otp is not None:
+            raise OTPGenerationFailedException
+        otp_code = self.__generate_otp_code()
+        self.hashed_otp = self.encoder.to_hash(password=otp_code)
+        return otp_code
+
     def __invalid_status_for_verification(self) -> bool:
         return self.status != OTPStatusEnum.PENDING.value
 
@@ -31,5 +43,5 @@ class OTP:
         match = self.encoder.compare(string=otp_code, hashed=self.hashed_otp)
         return not match
 
-    def __to_hash(self, otp_code: str) -> str:
-        return self.encoder.to_hash(password=otp_code)
+    def __generate_otp_code(self) -> str:
+        return str(randint(0, 999999)).zfill(6)
---
+++ src/domain/otp/exceptions/otp_generation_failed_exception.py
@@ -0,0 +1,2 @@
+class OTPGenerationFailedException(Exception):
+    pass
