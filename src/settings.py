c72891af0caf2fa7caf4820e30ca51d728d315df
Bruno Lucattelli <lucattelli@gmail.com>
2021-01-17 00:32:41 -0300

    Fix settings usage

--- src/settings.py
+++ src/settings.py
@@ -1,9 +1,20 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path='.env', verbose=True)
+settings = {
+    'GMAIL_USER': os.getenv('GMAIL_USER', ''),
+    'GMAIL_PASSWORD': os.getenv('GMAIL_PASSWORD', ''),
+    'GMAIL_URL': os.getenv('GMAIL_URL', ''),
+    'GMAIL_PORT': os.getenv('GMAIL_PORT', ''),
+}
 
-GMAIL_USER = os.getenv("GMAIL_USER")
-GMAIL_PASSWORD = os.getenv("GMAIL_PASSWORD")
-GMAIL_URL = os.getenv("GMAIL_URL")
-GMAIL_PORT = os.getenv("GMAIL_PORT")
+
+def load_settings():
+    load_dotenv(dotenv_path='src/.env', verbose=True)
+    settings['GMAIL_USER'] = os.getenv('GMAIL_USER', '')
+    settings['GMAIL_PASSWORD'] = os.getenv('GMAIL_PASSWORD', '')
+    settings['GMAIL_URL'] = os.getenv('GMAIL_URL', '')
+    settings['GMAIL_PORT'] = os.getenv('GMAIL_PORT', '')
+
+
+load_settings()
