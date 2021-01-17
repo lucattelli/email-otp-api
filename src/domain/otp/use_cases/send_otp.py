d9baaab8e1af9b7d72bfd47f6a43ff3422a3671f
Bruno Lucattelli <lucattelli@gmail.com>
2021-01-17 01:04:19 -0300

    Added verify OTP use case

--- src/domain/otp/use_cases/send_otp.py
+++ src/domain/otp/use_cases/send_otp.py
@@ -1,3 +1,4 @@
+from typing import Dict
 from domain.otp.entities.email.mailer_abstract import MailerAbstract
 from domain.otp.entities.otp.hash_abstract import HashAbstract
 from domain.otp.entities.email.email import Email
@@ -5,6 +6,9 @@ from domain.otp.entities.otp.otp import OTP
 from domain.otp.entities.otp.otp_repository_abstract import OTPRepositoryAbstract
 from domain.otp.enums.otp_method_enum import OTPMethodEnum
 from domain.otp.enums.otp_status_enum import OTPStatusEnum
+from domain.otp.exceptions.otp_generation_failed_exception import (
+    OTPGenerationFailedException,
+)
 
 
 class SendOTP:
@@ -19,10 +23,15 @@ class SendOTP:
         self.mailer = mailer
         self.otp = self.__build_otp(encoder=encoder, to=to)
 
-    def execute(self) -> None:
-        otp_code = self.otp.generate_otp()
-        email = self.__build_email(otp_code=otp_code)
-        email.send()
+    def execute(self) -> Dict:
+        try:
+            otp_code = self.otp.generate_otp()
+            email = self.__build_email(otp_code=otp_code)
+            email.send()
+            self.repository.save(self.otp)
+            return {'status': self.otp.status, 'ttl_seconds': self.otp.ttl}
+        except OTPGenerationFailedException as e:
+            return {'status': f'failed: {e}'}
 
     def __build_otp(self, encoder: HashAbstract, to: str) -> OTP:
         return OTP(
